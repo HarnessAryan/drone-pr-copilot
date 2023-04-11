1f08f31b7ea38a3a8c8e822f1d45bc0975394340
Vistaar Juneja <vistaar.juneja@harness.io>
2023-04-11 12:47:14 +0100

    update file struct

--- plugin/types.go
+++ plugin/types.go
@@ -2,19 +2,21 @@ package plugin
 
 // File represents a list of lines in the pull request
 type File struct {
-	Name  string
-	Lines []Line
+	Name          string
+	PreviousLines []Line
+	DiffLines     []Line
 }
 
 type Line struct {
 	Number  int
 	Content string
+	Removed bool // whether the line was added or removed, default: false
 }
 
 // Feedback is what we receive from OpenAI and comment back on the PR
 type Feedback struct {
-	Filename   string
-	LineNumber int
-	Suggestion string
-	Severity   string // optional
+	Filename   string `json:"filename"`
+	LineNumber int    `json:"line_number"`
+	Suggestion string `json:"suggestion"`
+	Severity   string `json:"severity"` // optional
 }
