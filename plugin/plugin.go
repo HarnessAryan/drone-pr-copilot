7476520471fbb223c1ffeb1c9d5d4bd1882f567f
Vistaar Juneja <vistaar.juneja@harness.io>
2023-04-12 14:53:35 +0100

    get pr number from commit link

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -8,6 +8,8 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
 
 	"github.com/google/go-github/v41/github"
 	"golang.org/x/oauth2"
@@ -29,11 +31,17 @@ type Args struct {
 func Exec(ctx context.Context, args Args) error {
 	// Printing some data that we will need
 	fmt.Println("pipeline namespace: ", args.Pipeline.Repo.Namespace)
-	fmt.Println("pr number: ", args.Pipeline.Build.Number)
+	tokens := strings.Split(args.Pipeline.Commit.Link, "/")
+	prs := tokens[len(tokens)-1]
+	pr, err := strconv.Atoi(prs)
+	if err != nil {
+		log.Fatalf("could not parse pr number")
+	}
+	fmt.Println("pr number: ", pr)
 	fmt.Println("pipeline repo name: ", args.Pipeline.Repo.Name)
 	githubClient := createGithubClient(ctx, args)
 
-	fileDiffs, err := GetFileDiff(ctx, githubClient, args.Pipeline.Repo.Namespace, args.Pipeline.Repo.Name, args.Pipeline.Build.Number)
+	fileDiffs, err := GetFileDiff(ctx, githubClient, args.Pipeline.Repo.Namespace, args.Pipeline.Repo.Name, pr)
 	if err != nil {
 		log.Fatalf("could not get file diff, err: %s", err)
 	}
@@ -42,7 +50,7 @@ func Exec(ctx context.Context, args Args) error {
 	openAIClient := New(WithToken(args.OpenAIKey))
 	feedback := openAIClient.Feedback(ctx, fileDiffs)
 
-	err = postReviewComment(ctx, githubClient, args.Pipeline.Repo.Namespace, args.Pipeline.Repo.Name, args.Pipeline.Build.Number, feedback)
+	err = postReviewComment(ctx, githubClient, args.Pipeline.Repo.Namespace, args.Pipeline.Repo.Name, pr, feedback)
 	if err != nil {
 		log.Fatalf("could not post review comments, err: %s", err)
 	}
